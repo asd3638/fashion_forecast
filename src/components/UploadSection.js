af13f2e9df405338ec449199ef31236a0ddb76c1
heejaykong <heejay.dev@gmail.com>
2021-09-16 17:48:50 +0900

    move ResultSection into UploadSection

--- src/components/UploadSection.js
+++ src/components/UploadSection.js
@@ -3,6 +3,7 @@ import { StyledBase } from "../global-styles";
 import InputBox from "../components/InputBox";
 import React, { useState, useEffect } from "react";
 import api from "../Api/api";
+const dummyOutfits = ["긴팔 니트웨어", "7부 데님", "긴팔 무스탕"];
 
 const Wrapper = styled.section`
   display: flex;
@@ -35,38 +36,60 @@ const SeeResultBtn = styled(StyledBase)`
   width: 24rem;
   height: 4.5rem;
 `;
+const ResultContainer = styled.section`
+  white-space: pre;
+  & .outfit-prediction {
+    line-height: 110%;
+    margin-bottom: 3rem;
+  }
+  & .result {
+    & > p {
+      line-height: 200%;
+    }
+    & > p > span {
+      margin-top: 1rem;
+      font-size: 3rem;
+      font-weight: 700;
+    }
+  }
+  @media screen and (min-width: 612px) {
+    display: flex;
+    flex-direction: column;
+    padding: 0 12rem;
+  }
+`;
 
 function UploadSection() {
   const formData = new FormData();
 
   const handleUpload = (img, kind) => {
     if (kind === "top") {
-      formData.append("top", img)
+      formData.append("top", img);
     }
     if (kind === "bottom") {
-      formData.append("bottom", img)
+      formData.append("bottom", img);
     }
     if (kind === "outer") {
-      formData.append("outer", img)
+      formData.append("outer", img);
     }
     if (kind === "op") {
-      formData.append("op", img)
+      formData.append("op", img);
     }
-  }
+  };
 
   const onSubmitHandler = (e) => {
     const config = {
       header: {
-        'Content-Type':'multipart/form-data'
-      }
-    }
+        "Content-Type": "multipart/form-data",
+      },
+    };
     e.preventDefault();
     api
       .post("/upload/post", formData, config)
       .then((res) => {
         // 서버 작업 성공하면
-        if (res.status  === 200) {
-          console.log(res.data)
+        if (res.status === 200) {
+          console.log(res.data);
         }
       })
       .catch();
@@ -78,16 +101,33 @@ function UploadSection() {
         <h1 class="quesetion">무엇을 입을 예정인가요?</h1>
         <div class="input-box-group">
           <div class="input-box-row">
-            <InputBox title="상의" kind="top" handleUpload={handleUpload}/>
-            <InputBox title="하의" kind="bottom" handleUpload={handleUpload}/>
+            <InputBox title="상의" kind="top" handleUpload={handleUpload} />
+            <InputBox title="하의" kind="bottom" handleUpload={handleUpload} />
           </div>
           <div class="input-box-row">
-            <InputBox title="아우터" kind="outer" handleUpload={handleUpload}/>
-            <InputBox title="원피스" kind="op" handleUpload={handleUpload}/>
+            <InputBox title="아우터" kind="outer" handleUpload={handleUpload} />
+            <InputBox title="원피스" kind="op" handleUpload={handleUpload} />
           </div>
         </div>
-        <SeeResultBtn buttonStyle onClick={onSubmitHandler}>결과 보기</SeeResultBtn>
+        <SeeResultBtn buttonStyle onClick={onSubmitHandler}>
+          결과 보기
+        </SeeResultBtn>
       </Wrapper>
+
+      <ResultContainer>
+        <p class="outfit-prediction">
+          {`${dummyOutfits.join(",\n")}을(를) 입으셨군요?`}
+        </p>
+        <div class="result">
+          <p>
+            지금 옷차림은 날씨에...
+            <br />
+            <span>적합</span>합니다.
+            <br />
+            좋은 하루 보내세요!
+          </p>
+        </div>
+      </ResultContainer>
     </>
   );
 }
