628ac026c89f638f1000fe1d4452099761ea4bcb
heejaykong <heejay.dev@gmail.com>
2021-10-15 21:25:50 +0900

    fix untracked files

--- src/routes/Login.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import LoginForm from "../components/LoginForm";
-
-const Signin = () => {
-  return <LoginForm />;
-};
-export default Signin;
--- src/routes/Signin.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import SigninForm from "../components/SigninForm";
-
-const Signin = () => {
-  return <SigninForm />;
-};
-export default Signin;
